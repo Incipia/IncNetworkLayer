8a84f1fb424a922f19c1f2c7cd5a24be71635fa3
Leif Meyer <leif@incipia.co>
2017-06-12 11:40:59 -0700

    Enabled IncNetworkQueue to manage the app's network activity indicator.

--- IncNetworkLayer/Classes/IncNetworkQueue.swift
+++ IncNetworkLayer/Classes/IncNetworkQueue.swift
@@ -1,14 +1,26 @@
 import Foundation
 
-public class IncNetworkQueue {
+public class IncNetworkQueue: NSObject {
    
    public static var shared: IncNetworkQueue!
    
    let queue = OperationQueue()
+   public var managesNetworkActivityIndicator: Bool = false
    
-   public init() {}
+   public override init() {
+      super.init()
+      queue.addObserver(self, forKeyPath: #keyPath(OperationQueue.operationCount), options: [.new], context: nil)
+   }
    
    public func addOperation(_ op: Operation) {
       queue.addOperation(op)
+   }
+   
+   // MARK: - KVO
+   public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+      guard managesNetworkActivityIndicator else { return }
+      guard let count = change?[.newKey] as? Int else { fatalError() }
+      
+      UIApplication.shared.isNetworkActivityIndicatorVisible = count > 0
    }
 }
