0a31a5e59fae7796b957ed8edd2f301e16d0a665
Leif Meyer <leif@incipia.co>
2017-06-13 08:09:23 -0700

    Added network activity start and stop notifications to IncNetworkQueue.

--- IncNetworkLayer/Classes/IncNetworkQueue.swift
+++ IncNetworkLayer/Classes/IncNetworkQueue.swift
@@ -1,6 +1,17 @@
 import Foundation
 
-public class IncNetworkQueue: NSObject {
+public enum IncNetworkQueueNotification: String, IncNotificationType {
+   case startedNetworkActivity, stoppedNetworkActivity
+}
+
+public class IncNetworkQueue: NSObject, IncNotifier {
+   // MARK: - Types
+   public enum Notification: String, IncNotificationType {
+      case startedNetworkActivity, stoppedNetworkActivity
+      
+      public static var namePrefix: String { return "IncNetworkQueue" }
+   }
+   
    // MARK: - Singleton
    public static var shared: IncNetworkQueue!
    
@@ -11,7 +22,7 @@ public class IncNetworkQueue: NSObject {
    // MARK: - Init
    public override init() {
       super.init()
-      queue.addObserver(self, forKeyPath: #keyPath(OperationQueue.operationCount), options: [.new], context: nil)
+      queue.addObserver(self, forKeyPath: #keyPath(OperationQueue.operationCount), options: [.new, .old], context: nil)
    }
    
    // MARK: - Public
@@ -21,9 +32,16 @@ public class IncNetworkQueue: NSObject {
    
    // MARK: - KVO
    public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-      guard managesNetworkActivityIndicator else { return }
       guard let count = change?[.newKey] as? Int else { fatalError() }
+      guard let oldCount = change?[.oldKey] as? Int else { fatalError() }
       
+      if oldCount == 0, count > 0 {
+         post(notification: .startedNetworkActivity)
+      } else if oldCount > 0, count == 0 {
+         post(notification: .stoppedNetworkActivity)
+      }
+      
+      guard managesNetworkActivityIndicator else { return }
       UIApplication.shared.isNetworkActivityIndicatorVisible = count > 0
    }
    
