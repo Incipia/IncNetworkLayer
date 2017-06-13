b00f5dafe549e1f5bde021607938c309750573bd
Leif Meyer <leif@incipia.co>
2017-06-13 06:52:49 -0700

    Fixed a bug that caused tests to crash because the network queue was not removing its key-value observation.

--- IncNetworkLayer/Classes/IncNetworkQueue.swift
+++ IncNetworkLayer/Classes/IncNetworkQueue.swift
@@ -1,17 +1,20 @@
 import Foundation
 
 public class IncNetworkQueue: NSObject {
-   
+   // MARK: - Singleton
    public static var shared: IncNetworkQueue!
    
+   // MARK: - Public Properties
    let queue = OperationQueue()
    public var managesNetworkActivityIndicator: Bool = false
    
+   // MARK: - Init
    public override init() {
       super.init()
       queue.addObserver(self, forKeyPath: #keyPath(OperationQueue.operationCount), options: [.new], context: nil)
    }
    
+   // MARK: - Public
    public func addOperation(_ op: Operation) {
       queue.addOperation(op)
    }
@@ -22,5 +25,10 @@ public class IncNetworkQueue: NSObject {
       guard let count = change?[.newKey] as? Int else { fatalError() }
       
       UIApplication.shared.isNetworkActivityIndicatorVisible = count > 0
+   }
+   
+   // MARK: - Deinit
+   deinit {
+      queue.removeObserver(self, forKeyPath: #keyPath(OperationQueue.operationCount))
    }
 }
