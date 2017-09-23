9af7665d78ab52c5a4754c2a74d9ce7bf1ded2c5
Leif Meyer <leif@incipia.co>
2017-09-23 11:47:10 -0700

    Made the baseURL property of IncNetworkRequestConfiguration public.

--- IncNetworkLayer/Classes/Request/IncNetworkRequestConfiguration.swift
+++ IncNetworkLayer/Classes/Request/IncNetworkRequestConfiguration.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public final class IncNetworkRequestConfiguration {
    
-   let baseURL: URL
+   public let baseURL: URL
    
    public init(baseURL: URL) {
       self.baseURL = baseURL
