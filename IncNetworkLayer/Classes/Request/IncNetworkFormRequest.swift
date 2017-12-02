02181766648fc2696ec5c33ad166bdaad9e0d02a
Leif Meyer <leif@incipia.co>
2017-12-02 11:44:47 -0800

    Changed IncNetworkFormRequest to permit file upload as application/octet-stream.

--- IncNetworkLayer/Classes/Request/IncNetworkFormRequest.swift
+++ IncNetworkLayer/Classes/Request/IncNetworkFormRequest.swift
@@ -11,11 +11,17 @@ public struct IncNetworkFormFileParameter {
    public var fileName: String
    public var data: Data
    public var contentType: String
+   public var contentTransferEncoding: String?
    
-   public init(fileName: String, data: Data, contentType: String) {
+   public init(fileName: String, data: Data, contentType: String, contentTransferEncoding: String? = nil) {
       self.fileName = fileName
       self.data = data
       self.contentType = contentType
+      self.contentTransferEncoding = contentTransferEncoding
+   }
+   
+   public init(fileName: String, data: Data) {
+      self.init(fileName: fileName, data: data, contentType: "application/octet-stream")
    }
 }
 
@@ -37,49 +43,72 @@ extension IncNetworkFormRequest {
    }
    
    public func body(parameters: [String : Any]?, encoding: String.Encoding = .utf8, isURLEncoded: Bool = true, boundary: inout String?) throws -> Data? {
-      var optionalBody: String?
+      var optionalBody: Data?
       if isURLEncoded {
+         var optionalBodyString: String?
          do {
-            optionalBody = try self.query(with: parameters)
+            optionalBodyString = try self.query(with: parameters)
          } catch {
             switch error {
             case IncNetworkRequestError.invalidQueryParameter(let name): throw IncNetworkRequestError.invalidBodyParameter(name: name)
             default: throw error
             }
          }
+         if let bodyString = optionalBodyString {
+            guard let data = bodyString.data(using: encoding) else { throw IncNetworkRequestError.invalidBody(encoding: encoding) }
+            optionalBody = data
+         }
       } else if let parameters = parameters, !parameters.isEmpty {
-         var bodyParts: [String] = []
+         var bodyDataParts: [Data] = []
          
          for param in parameters {
             var body = ""
             let paramName = param.key
             body += "Content-Disposition:form-data; name=\"\(paramName)\""
             if let fileParam = param.value as? IncNetworkFormFileParameter {
-               let contentType = fileParam.contentType
-               let fileContent = fileParam.data.base64EncodedString()
                body += "; filename=\"\(fileParam.fileName)\"\r\n"
-               body += "Content-Type: \(contentType)\r\n\r\n"
-               body += fileContent
+               body += "Content-Type: \(fileParam.contentType)\r\n\r\n"
+               if let contentTransferEncoding = fileParam.contentTransferEncoding {
+                  body += "Content-Transfer-Encoding: \(contentTransferEncoding)\r\n\r\n"
+               }
+
+               guard var data = body.data(using: encoding) else { throw IncNetworkRequestError.invalidBody(encoding: encoding) }
+               data.append(fileParam.data)
+               bodyDataParts.append(data)
             } else {
                body += "\r\n\r\n\(param.value)"
+               guard let data = body.data(using: encoding) else { throw IncNetworkRequestError.invalidBody(encoding: encoding) }
+               bodyDataParts.append(data)
             }
-            bodyParts.append(body)
          }
          
-         let boundary = boundary ?? self.boundary(bodyParts: bodyParts)
-         optionalBody = "--\(boundary)\r\n\(bodyParts.joined(separator: "\r\n--\(boundary)\r\n"))\r\n--\(boundary)--"
+         let boundary = boundary ?? self.boundary(bodyParts: bodyDataParts)
+         guard let initialBoundryData = "--\(boundary)\r\n".data(using: encoding),
+         let midBoundryData = "\r\n--\(boundary)\r\n".data(using: encoding),
+         let finalBoundryData = "\r\n--\(boundary)--".data(using: encoding) else { throw IncNetworkRequestError.invalidBody(encoding: encoding) }
+         var data = Data()
+         data.append(initialBoundryData)
+         for (index, dataPart) in bodyDataParts.enumerated() {
+            data.append(dataPart)
+            if index < bodyDataParts.count - 1 {
+               data.append(midBoundryData)
+            }
+         }
+         data.append(finalBoundryData)
+         optionalBody = data
       }
       
       guard let body = optionalBody else { return nil }
-      guard let data = body.data(using: encoding) else { throw IncNetworkRequestError.invalidBody(encoding: encoding) }
+      print("request body: \(String(data: body, encoding: .ascii)!)")
       
-      return data
+      return body
    }
 
-   public func boundary(bodyParts: [String] = []) -> String {
+   public func boundary(bodyParts: [Data] = []) -> String {
       var boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW"
       var countDown = UInt32.max
-      while bodyParts.joined().range(of: boundary) != nil, countDown > 0 {
+      let bodyString = bodyParts.map { String(data: $0, encoding: .ascii)! }.joined()
+      while bodyString.range(of: boundary) != nil, countDown > 0 {
          boundary = "----WebKitFormBoundary\(arc4random())"
          countDown -= 1
       }
