802b241bc710f25b3a6b38a89fa89f36f98adba9
Leif Meyer <leif@incipia.co>
2018-02-06 09:23:59 -0800

    Added the request method to IncNetworkService's logging output.

--- IncNetworkLayer/Classes/Network/IncNetworkService.swift
+++ IncNetworkLayer/Classes/Network/IncNetworkService.swift
@@ -43,7 +43,7 @@ public class IncNetworkService {
             return
          }
          guard let httpResponse = response as? HTTPURLResponse else {
-            print("Request to URL \(url) finished with non-HTTP response.")
+            print("Request to \(method) \(url) finished with non-HTTP response.")
             success?(data, response, .nonHTTP)
             return
          }
@@ -51,13 +51,13 @@ public class IncNetworkService {
          let statusCode = httpResponse.statusCode
          switch statusCode {
          case let statusCode where self._successCodes.contains(statusCode):
-            print("Request to URL \(url) finished with success code \(statusCode).")
+            print("Request to \(method) \(url) finished with success code \(statusCode).")
             success?(data, response, .httpSuccess(code: statusCode))
          case let statusCode where self._failureCodes.contains(statusCode):
-            print("Request to URL \(url) finished with failure code \(statusCode).")
+            print("Request to \(method) \(url) finished with failure code \(statusCode).")
             failure?(data, response, .httpFailure(code: statusCode))
          default:
-            print("Request to URL \(url) finished with unexpected code \(statusCode).")
+            print("Request to \(method) \(url) finished with unexpected code \(statusCode).")
             // Server returned response with status code different than
             // expected `successCodes`.
             failure?(data, response, .unexpectedStatus(code: statusCode))
